e91e19face870c348b0b6f5c6aad24adce6991b8
FerSo22 <20a301073@untels.edu.pe>
2022-11-03 11:00:41 -0500

    Aplicación con Modularidad Lista

--- aplicacion.py
+++ aplicacion.py
@@ -1,3 +1,5 @@
+import gestion_personas
+
 def validar_login():
     login = open("login.txt", encoding="utf8")
     usr = login.read()
@@ -15,7 +17,33 @@ def menu():
     print("1. Listar personas"
           + "\n2. Agregar personas"
           + "\n3. Salir")
- 
+    opcion = int(input("Digite la opción: "))
+
+    if opcion == 1:
+        dni = gestion_personas.listar_personas("dni")
+        nombres = gestion_personas.listar_personas("nombre")
+        apellidos = gestion_personas.listar_personas("apellido")
+
+        print("\t\t\t\t\tLISTADO DE PERSONAS")
+        print("\nOrden\t\t\t\tN° de DNI\t\t\t\tNombres\t\t\t\tApellidos")
+        for n in range(len(dni)):
+            print("  " + str(n + 1) + "  \t\t\t\t" + dni[n].strip() + "\t\t\t\t" + nombres[n].strip() + "\t\t\t\t" +
+                  apellidos[n].strip())
+
+    if opcion == 2:
+        print("\n\tAGREGAR DATOS DE PERSONAS")
+        nuevo_dni = input("Digite el nuevo N° de DNI: ")
+        nuevo_nombres = input("Digite los nuevos nombres: ")
+        nuevo_apellidos = input("Digite los nuevos apellidos: ")
+
+        gestion_personas.agregar_personas("dni", nuevo_dni)
+        gestion_personas.agregar_personas("nombre", nuevo_nombres)
+        gestion_personas.agregar_personas("apellido", nuevo_apellidos)
+
+    if opcion == 3:
+        print("\nPROGRAMA FINALIZADO CON ÉXITO")
+        return
+
 def iniciar_sesion(intentos = 0):
     user = input("\nDigite el nombre de usuario: ")
     password = input("Digite la contraseña: ")
